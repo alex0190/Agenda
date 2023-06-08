ead856e1b44eb793743e3cd6c842baf200503007
Marcos Gallardo <socramg@gmail.com>
2023-06-07 21:01:01 -0300

    Nueva reescritura

--- router.php
+++ router.php
@@ -2,37 +2,43 @@
 include_once("includes/config.php");
 include_once("includes/clases/class.Contacto.php");
 
-$entidad = $_GET["var1"];
-$accion = $_GET["var2"];
-$id = $_GET["var3"];
+// Analizar URL
+$uri = $_SERVER['REQUEST_URI'];
+// Eliminar parámetros de consulta
+if ($pos = strpos($uri, '?')) $uri = substr($uri, 0, $pos);
+// Eliminar la ruta raíz
+$path = substr($uri, strlen(dirname($_SERVER['PHP_SELF'])) + 1);
+unset($uri, $pos);
 
-if(empty($entidad))
-	$path = "index";
-else
-{
-	if(strpos($entidad, ".") == false)
-	{
-	    if(file_exists($entidad.".php"))
-	    {
-		   // Se accede al archivo php principal del sitio
-		
-		   $path = $entidad;
+// Termina en /
+if (substr($path, -1) === '/') $path = substr($path, 0, -1);
+
+// Obtenemos los parámetros de la ruta
+$params = explode('/', $path);
+$entidad = $params[0] ?? '';
+$accion = $params[1] ?? '';
+$id = $params[2] ?? '';
+unset($params, $path);
+
+// 
+if (empty($entidad)) $path = "index";
+else {
+	if (strpos($entidad, ".") == false) {
+		if (file_exists($entidad . ".php")) {
+			// Se accede al archivo php principal del sitio
+
+			$path = $entidad;
+		} else if (file_exists($entidad)) {
+			// Se accede a alguna subcarpeta del directorio principal del sitio
+
+			$path = $entidad . "/" . $accion;
 		}
-	    else if(file_exists($entidad))
-	    {
-	       // Se accede a alguna subcarpeta del directorio principal del sitio
-	   
-           $path = $entidad."/".$accion;	   
-		}	
-	}
-	else
-	{
+	} else {
 		// Se accede a alguna subcarpeta del directorio includes/acciones
-		
+
 		$entidades = explode(".", $entidad);
-		$path = "includes/".$entidades[0]."/".$entidades[1]."/".$accion;
+		$path = "includes/" . $entidades[0] . "/" . $entidades[1] . "/" . $accion;
 	}
 }
 
-include("./".$path.".php");
-?>
+include("./" . $path . ".php");
